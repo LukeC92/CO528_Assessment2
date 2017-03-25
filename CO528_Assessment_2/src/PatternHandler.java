3f6a0d9498bc446ec5b4464f5d88bbe2908e2ae6
LukeCarroll <luke.carroll@live.co.uk>
2017-03-25 18:36:40 +0000

    Starting to make classes with unittests.

--- CO528_Assessment_2/src/PatternHandler.java
+++ CO528_Assessment_2/src/PatternHandler.java
@@ -6,26 +6,22 @@
  * @author Luke Carroll (lcc29)
  *
  */
-public class Hopf {
+public class PatternHandler {
 
 	/**
-	 * @param args
+	 * Creates the PatternHandler object.
 	 */
-	public static void main(String[] args) {
-		System.out.println(splitPattern("1 2 3")[0]);
-		System.out.println(splitPattern("1 2 3")[1]);
-		System.out.println(splitPattern("-1 1 1")[0]);
-		System.out.println("1 2 3".split(" ")[0]);
-		
+	public PatternHandler() {
 
 	}
 
 	/**
 	 * Splits a pattern which comes as a string into an array of integers
+	 * 
 	 * @param input The input Pattern as a string
 	 * @return The pattern as an array of integers
 	 */
-	private static int[] splitPattern(String input) {
+	public int[] splitPattern(String input) {
 		String[] strArray = input.split(" ");
 		int[] intArray = new int[strArray.length];
 		for (int i = 0; i < strArray.length; i++) {
@@ -33,8 +29,5 @@ public class Hopf {
 		}
 		return intArray;
 	}
-	
-	
-	
 
 }
---
+++ CO528_Assessment_2/src/unittesting/PatternHandlerTesting.java
@@ -0,0 +1,18 @@
+package unittesting;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PatternHandlerTesting {
+
+	/*
+	@Test
+	public void test() {
+		PatternHandler ph = new PatternHandler();
+		fail("Not yet implemented");
+	}
+	*/
+
+
+}
