4135cb2361e6a6d3929c2821046a9b97dc56d8ff
LukeCarroll <luke.carroll@live.co.uk>
2017-03-25 16:12:21 +0000

    Start of CO528 Assessment 2.

---
+++ CO528_Assessment_2/src/Hopf.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Luke Carroll (lcc29)
+ *
+ */
+public class Hopf {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
